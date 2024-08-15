39b7f5bda4510d64e148e758cbc2dcde421b845f
Markos Alves pereira <101573252+Markosalves12@users.noreply.github.com>
2024-08-15 18:47:41 -0300

    Add files via upload

---
+++ multiplas_requisicoes.py
@@ -0,0 +1,37 @@
+import asyncio
+import aiohttp
+
+
+class MultipleRequisition:
+    def __init__(self, urls, batch_size = 31):
+        # lista de urls para serem solcitadas
+        self.urls = urls
+        # tamanho do lote de url
+        self.batch_size = batch_size
+
+    async def make_requisition(self, session, url):
+        async with session.get(url) as response:
+            # Lê o conteúdo da resposta e faz o parsing para JSON
+            return await response.json()
+
+    async def make_batch_requisition(self, session, batch_urls):
+        tasks = [self.make_requisition(session, url) for url in batch_urls]
+        return await asyncio.gather(*tasks)
+
+    async def make_assincronous_requisition(self):
+        async with aiohttp.ClientSession() as session:
+            # Dividir as URLs em lotes
+            batches = [self.urls[i:i + self.batch_size] for i in range(0, len(self.urls), self.batch_size)]
+
+            # Fazer requisições em lotes
+            results = []
+            for batch in batches:
+                results.extend(await self.make_batch_requisition(session, batch))
+
+            # retorna uma lista de JSONS com as respostas disponiveis
+            return results
+
+    def make_requisitions(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        return loop.run_until_complete(self.make_assincronous_requisition())
